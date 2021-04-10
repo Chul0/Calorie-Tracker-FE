00a0d64be2363edfbf002bf8516a087d684bf766
parksungchul <parksungchul@Sungchul-MacBook-Pro.local>
2021-04-10 18:16:32 -0400

    clogin/singup changed, logout added

--- main.js
+++ main.js
@@ -1,5 +1,54 @@
+const showSection = (sectionClass) => {
+    document.querySelectorAll('section').forEach( s => s.classList.add('hidden'))
+    document.querySelector(sectionClass).classList.remove('hidden')
+}
+
+const showLoginBoard = () => {
+    showSection('.login-board')
+}
+const showSignUpBoard = () => {
+    showSection('.signup-board')
+}
+const showDashBoard = () => {
+    showSection('.dashboard')
+}
+
+const loggedIn = () => {
+    document.querySelector('#login-link').classList.add('hidden')
+    document.querySelector('#signup-link').classList.add('hidden')  //logged in 
+    document.querySelector('#logout-link').classList.remove('hidden')
+    document.querySelector('#mymeal-link').classList.remove('hidden') 
+    document.querySelector('#profile-link').classList.remove('hidden') 
+}
+const loggedOut = () => {
+    document.querySelector('#logout-link').classList.add('hidden')
+    document.querySelector('#mymeal-link').classList.add('hidden') 
+    document.querySelector('#profile-link').classList.add('hidden') 
+    document.querySelector('.login-board').classList.add('hidden') 
+    document.querySelector('.signup-board').classList.add('hidden') 
+    document.querySelector('.dashboard').classList.add('hidden') //show logged out
+}
+
+
+//Changes based on login/logout statement
+if(localStorage.getItem('userId')){
+    loggedIn()
+    showDashBoard()
+    //if present, show logged in state
+  }else{
+    loggedOut()
+  }
+
+
+//When Home is clicked, page refreshes
+document.querySelector('#home-link').addEventListener('click', () => {
+    location.reload();
+})
+
 
 //signup
+document.querySelector('#signup-link').addEventListener('click', () => {
+    showSection('.signup-board')
 document.querySelector('.signup-form').addEventListener('submit', async (event) => {
     event.preventDefault()
     const firstName = document.querySelector('#signup-first-name').value
@@ -25,14 +74,18 @@ document.querySelector('.signup-form').addEventListener('submit', async (event) 
         }else{
             const userId = response.data.user.id
             localStorage.setItem('userId', userId)
+            showLoginBoard()
         }
     } catch (error) {
         // console.log(error)
             alert('email is already used by someone')
     }
 })
+})
 
 //login
+document.querySelector('#login-link').addEventListener('click', () => {
+    showSection('.login-board')
 document.querySelector('#login-form').addEventListener('submit', async (event) => {
     event.preventDefault()
     const email = document.querySelector('#login-email').value
@@ -46,12 +99,21 @@ try {
     console.log(response)
 
     const userId = response.data.user.id
-    localStorage.setItem('userId', userId) //'userId' is like a variable, can have any name 
+    localStorage.setItem('userId', userId) //'userId' is like a variable, can name it with anything
 
     alert(`Welcome back ${response.data.user.firstName} !`)
-    
+    showDashBoard()
+    loggedIn()
+
 } catch (error) {
     console.log(error)
     alert('email or password is incorrect')
 }
+})
+})
+
+//Logout
+document.querySelector('#logout-link').addEventListener('click', () =>{
+    localStorage.removeItem('userId')
+    location.reload();
 })
