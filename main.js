7dacc8430f784c8f69ce5b672108afbc52f64455
parksungchul <parksungchul@Sungchul-MacBook-Pro.local>
2021-04-10 01:25:42 -0400

    CRUD[login]

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+//signup
 document.querySelector('.signup-form').addEventListener('submit', async (event) => {
     event.preventDefault()
     const firstName = document.querySelector('#signup-first-name').value
@@ -28,4 +30,28 @@ document.querySelector('.signup-form').addEventListener('submit', async (event) 
         // console.log(error)
             alert('email is already used by someone')
     }
+})
+
+//login
+document.querySelector('#login-form').addEventListener('submit', async (event) => {
+    event.preventDefault()
+    const email = document.querySelector('#login-email').value
+    const password = document.querySelector('#login-password').value
+
+try {
+    const response = await axios.post('http://localhost:3001/users/login', {
+        email: email,
+        password: password
+    })
+    console.log(response)
+
+    const userId = response.data.user.id
+    localStorage.setItem('userId', userId) //'userId' is like a variable, can have any name 
+
+    alert(`Welcome back ${response.data.user.firstName} !`)
+    
+} catch (error) {
+    console.log(error)
+    alert('email or password is incorrect')
+}
 })
